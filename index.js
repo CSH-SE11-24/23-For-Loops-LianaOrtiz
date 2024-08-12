f6379cacd374d8a165e7ebdab57816a41359c284
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:37:42 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const prompt = require('prompt-sync')()
+// Task 1: Print numbers from 1 to 10 using a for loop.
+for(let i = 0; i < 11; i++){
+  console.log(i)
+}
+
+
+
+// Task 2: Print even numbers from 2 to 20 (inclusive) using a for loop.
+for(let o = 2; o < 22; o += 2){
+  console.log(o)
+}
+
+
+
+// Task 3: Calculate the sum of numbers from 10 to 20 using a for loop.
+
+let sum = 0
+for(let s = 10;  s <= 20; s++){
+sum+=s;
+}
+console.log("The sum is:", sum)
+
+
+
+
+// Task 4: Iterate over an array and print each element using a for loop.
+
+
+
